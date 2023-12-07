3e5f6b456a45f1e0f160b5a8aa75436904ef5dda
abramcikk <144155442+abramcikk@users.noreply.github.com>
2023-12-07 12:06:09 +0300

    cirqle

--- circle_test.py
+++ circle_test.py
@@ -7,7 +7,7 @@ import unittest
 class CircleTestCase(unittest.TestCase):
     def test_circle_area(self):
         res = area(10)
-        self.assertEqual(res, 314.159)
+        self.assertAlmostEqual(res, 314.159, delta=0.001)
     def test_circle_area_zero(self):
         res = area(0)
         self.assertEqual(res, 0)
@@ -16,10 +16,10 @@ class CircleTestCase(unittest.TestCase):
             area(-1)
     def test_circle_area_float(self):
         res = area(1.5)
-        self.assertEqual(res, 7.06)
+        self.assertAlmostEqual(res, 7.06, delta=0.01)
     def test_circle_perimeter(self):
         res = perimeter(10)
-        self.assertEqual(res, 62.83)
+        self.assertAlmostEqual(res, 62.83, delta = 0.01)
     def test_circle_perimeter_zero(self):
         res = perimeter(0)
         self.assertEqual(res, 0)
@@ -28,7 +28,7 @@ class CircleTestCase(unittest.TestCase):
             perimeter(-1)
     def test_circle_perimeter_float(self):
         res = perimeter(1.5)
-        self.assertEqual(res, 9.42)
+        self.assertAlmostEqual(res, 9.42, delta = 0.01)
 
 if __name__ == '__main__':
     unittest.main()
